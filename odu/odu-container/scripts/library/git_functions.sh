0e1c8e9d10d678ed2748b77bd1afdca37b8fc52f
gerardo-garcia <gerardo-garcia@users.noreply.github.com>
2024-05-29 09:22:52 +0200

    Add function config_local_git_user_email in git_functions.sh
    Signed-off-by: gerardo-garcia <gerardo-garcia@users.noreply.github.com>

--- odu/odu-container/scripts/library/git_functions.sh
+++ odu/odu-container/scripts/library/git_functions.sh
@@ -16,8 +16,16 @@ get_host() {
 }
 
 write_sshkey() {
+    echo "Writing GIT_SSHKEY in $HOME/.ssh/id_rsa"
     mkdir $HOME/.ssh
     echo "${GIT_SSHKEY}" | base64 -d > $HOME/.ssh/id_rsa
     chmod 700 $HOME/.ssh/id_rsa
+    echo "... DONE"
 }
 
+config_local_git_user_email() {
+    echo "Setting git local user and e-mail in current folder"
+    git config --local user.email "${GIT_USER_EMAIL}"
+    git config --local user.name "${GIT_USER_NAME}"
+    echo "... DONE"
+}
