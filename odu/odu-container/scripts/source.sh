ec79ce0238cb89a9a53382ba7ee0b3dfdf7bcf1d
gerardo-garcia <gerardo-garcia@users.noreply.github.com>
2024-05-30 14:21:45 +0200

    Update source.sh and test-environment.rc used for testing odu container
    Signed-off-by: gerardo-garcia <gerardo-garcia@users.noreply.github.com>

--- odu/odu-container/scripts/source.sh
+++ odu/odu-container/scripts/source.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex -o pipefail
 
 export GIT_REPO=${1:-${GIT_REPO}}
 export GIT_SSHKEY=${2:-${GIT_SSHKEY}}
@@ -6,11 +7,7 @@ export GIT_MANIFEST_FOLDER=${3:-${GIT_MANIFEST_FOLDER}}
 export GIT_PASSWORD=${GIT_PASSWORD}
 LOCAL_TEST_FLAG=${LOCAL_TEST_FLAG:-""}
 
-source $HOME/clone_git_repo.sh &> /dev/null
-
-set +x
+source $HOME/scripts/clone_git_repo.sh
 
 GIT_MANIFEST_FOLDER=${GIT_MANIFEST_FOLDER}
 kustomize cfg cat ${REPO_FOLDER}/${GIT_MANIFEST_FOLDER} --wrap-kind ResourceList --wrap-version config,kubernetes.io/v1alpha1
-
-
